c6f535778ab701913566247425392500212fb89a
Lukas Prochazka <prochazka.lu@gmail.com>
2023-02-14 12:23:28 +0100

    Add graphql server request

---
+++ util/client.ts
@@ -0,0 +1,41 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
+import { useMemo } from 'react';
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
+
+function createApolloClient() {
+  return new ApolloClient({
+    uri: 'https://api.github.com/graphql',
+    cache: new InMemoryCache(),
+    headers: {
+      authorization: `Bearer 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`,
+    },
+  });
+}
+
+export function initializeApollo(
+  initialState: NormalizedCacheObject | null = null,
+) {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
+  // get hydrated here
+  if (initialState) {
+    _apolloClient.cache.restore(initialState);
+  }
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === 'undefined') return _apolloClient;
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient;
+
+  return _apolloClient;
+}
+
+export function useApollo(initialState: NormalizedCacheObject) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+  return store;
+}
